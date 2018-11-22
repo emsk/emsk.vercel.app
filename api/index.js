b483d14f9d72694f846b0a6bff513058dcb80357
emsk <emsk1987@gmail.com>
2018-11-23 00:32:44 +0900

    Do not use unnecessary backquotes

--- api/index.js
+++ api/index.js
@@ -4,14 +4,14 @@ const app = express();
 const axios = require('axios');
 
 app.get('/code', async (req, res) => {
-  const url = `https://api.github.com/user/repos?visibility=public&affiliation=owner&sort=updated&direction=desc`;
+  const url = 'https://api.github.com/user/repos?visibility=public&affiliation=owner&sort=updated&direction=desc';
   const config = { headers: { 'Authorization': `token ${process.env.GITHUB_TOKEN}` } };
   const { data: repos } = await axios.get(url, config);
   res.send(repos);
 });
 
 app.get('/posts', async (req, res) => {
-  const url = `https://qiita.com/api/v2/authenticated_user/items`;
+  const url = 'https://qiita.com/api/v2/authenticated_user/items';
   const config = { headers: { 'Authorization': `Bearer ${process.env.QIITA_TOKEN}` } };
   const { data: items } = await axios.get(url, config);
   res.send(items);
