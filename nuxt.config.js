b18820669891cc1e6881cf3d6bfe0ea5598141bb
emsk <emsk1987@gmail.com>
2019-01-02 12:13:43 +0900

    Add Google Analytics module

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,8 @@ module.exports = {
     '@nuxtjs/axios',
     // Doc:https://github.com/nuxt-community/modules/tree/master/packages/bulma
     '@nuxtjs/bulma',
-    '@nuxtjs/dotenv'
+    '@nuxtjs/dotenv',
+    ['@nuxtjs/google-analytics', { id: 'UA-131553564-1' }]
   ],
   /*
   ** Axios module configuration
